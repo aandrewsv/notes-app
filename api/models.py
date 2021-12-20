c6718fd8856f25b5e3d6330cb66a038c46d5a4f2
aandrewsv <agustin.andrews@gmail.com>
2021-12-20 17:17:01 -0300

    Update note model, add tags and title

--- api/models.py
+++ api/models.py
@@ -3,10 +3,15 @@ from django.db import models
 
 
 class Note(models.Model):
+	TAG_CHOICES = [
+		('M', 'Money'),
+		('T', 'Todos'),
+		('R', 'Reminders'),
+		('W', 'Work'),
+		('D', 'Default'),
+	]
+	tag = models.textField(max_length=1, choices=TAG_CHOICES, default='D')
+	title = models.TextField(null=True, blank=True)
 	body = models.TextField(null=True, blank=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	created_at = models.DateTimeField(auto_now_add=True)
- 
-	def __str__(self):
-		return self.body[0:50]
-  
